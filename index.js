20f579715d105b99daf3a47e0ed548e467a0b648
Aodhagán Collins <aodhaganc@gmail.com>
2016-01-29 01:13:53 +0000

    Code: add main entry-point

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-(function() {
-  'use strict';
-
-  var http = require('http');
-  var mandrillWebhook = require('./src/mandrill-webhook.js');
-
-  var server = http.createServer(mandrillWebhook());
-
-  var port = 3000;
-
-  server.listen(port, function() {
-    console.log('Listening on port %s', port);
-  });
-
-})();
+module.exports = require('./src/mandrill-webhook-server.js');
